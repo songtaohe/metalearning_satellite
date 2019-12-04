3306f23d022a1e4123a516ea73f128cf163d8f75
songtao <songtao@mit.edu>
2019-12-04 00:05:49 -0500

    sync

--- MetalearningTest.py
+++ MetalearningTest.py
@@ -492,11 +492,11 @@ if __name__ == "__main__":
 
 	with tf.Session() as sess:
 		#model = MAML(sess,num_test_updates = 40,inner_lr=0.001)
-		model = MAMLFirstOrder20191119_pyramid(sess, num_test_updates = 2,inner_lr=0.001)
-		#model = MAMLFirstOrder20191119(sess, num_test_updates = 2,inner_lr=0.001)
+		#model = MAMLFirstOrder20191119_pyramid(sess, num_test_updates = 2,inner_lr=0.001)
+		model = MAMLFirstOrder20191119(sess, num_test_updates = 2,inner_lr=0.001)
 		
 		model.restoreModel(sys.argv[1])
-		model.update_parameters_after_restore_model() 
+		#model.update_parameters_after_restore_model() 
 
 		if len(sys.argv) > 2 :
 			model.restoreModel(sys.argv[2])
